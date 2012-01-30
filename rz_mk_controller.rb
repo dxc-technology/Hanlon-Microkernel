fb75dccc41ffac7fe30d8e7c1e02ee5692058239
Tom McSweeney <tjmcs@rumiel.(none)>
2012-01-30 13:06:05 -0800

    Refactored the rz_mk_controller.rb script (pulled out the code that waited for the network to be available into a separate utility class (which is defined in the new rz_network_utils.rb file) in order to make the rz_mk_controller a bit more generic

--- rz_mk_controller.rb
+++ rz_mk_controller.rb
@@ -1,69 +1,47 @@
 #!/usr/bin/env ruby
 
-eth_prefix = "eth"
-max_wait_time = 15 * 60   # wait for 15 minutes, max
-wait_between_iter = 15    # wait 15 seconds between iterations
-@start_time = Time.now.to_i
-
-def current_wait_time
-  Time.now.to_i - @start_time
-end
-
-# Set a couple of flags that will be used later on
-is_first_iter = true
-is_eth_up = false
-ip_is_valid = false
-
-# Start loop that will run until the network is available (or until the timeout
-# value is exceeded)
-begin
-  if !is_first_iter then
-    # skip this step, the first time through the loop
-    # puts "Network is not up yet, sleeping #{wait_between_iter} seconds..."
-    sleep wait_between_iter
-  else
-    # it's the first time through the loop, so set the flag
-    # to false (won't be the first time after this iteration)
-    is_first_iter = false
-  end
-
-  # loop through the entries in the output of the "ifconfig" command
-  # (under TCL, these entries are separated by a double-newline, so
-  # split the output of the "ifconfig" command on that string)
-  entries = %x[ifconfig].split("\n\n").each { |entry|
-    # for each entry, check for an ethernet adapter (to eliminate
-    # the loopback adapter) that has been assigned an IP address
-    # and is in an "UP" state
-    is_eth_up = (/#{eth_prefix}\d+/.match(entry) &&
-        /inet addr:\d+\.\d+\.\d+\.\d+\s+/.match(entry) &&
-        /UP/.match(entry))
-    # if we find an adapter that matches the criteria, above, then
-    # check to see if it has a valid IP address and break out of the
-    # inner loop (over ifconfig entries)
-    if is_eth_up then
-      # 169.xx.xx.xx type addresses indicate that the DHCP request failed
-      # to assign an address to the NIC in question (even though the adapter
-      # is up, it doesn't have a valid IP address assigned to it)
-      ip_is_valid = !/inet addr:169\.\d+\.\d+\.\d+\s+/.match(entry)
-      break
+# adds a "require_relative" function to the Ruby Kernel if it
+# doesn't already exist (used to deal with the fact that
+# "require" is used instead of "require_relative" prior
+# to Ruby v1.9.2)
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
     end
-  }
-
-end until (is_eth_up || current_wait_time >= max_wait_time)
-
-# Provide some diagnostic output if we are about to exit the loop
-if is_eth_up && ip_is_valid then
-  puts "Network is available, proceeding..."
-elsif !is_eth_up then
-  puts "Maximum wait time exceeded, network not found, exiting..."
-  exit(-1)
-else
-  puts "DHCP address assignment failed, exiting..."
-  exit(-2)
+  end
 end
+
+require_relative 'rz_network_utils'
+
+nw_is_avail = false
+
+rz_nw_util = RzNetworkUtils.new
+error_cond = rz_nw_util.wait_until_nw_avail
+nw_is_avail = true if error_cond == 0
 
 # add services to start once the network is up here...these services will
 # only run once the network is available
-t = %x[sudo env RUBYLIB=/usr/local/lib/ruby/1.8:/usr/local/mcollective/lib \
-  mcollectived --config /usr/local/etc/mcollective/server.cfg \
-  --pidfile /var/run/mcollective.pid]
+
+if nw_is_avail then
+
+  # sleep 15 more seconds, just in case
+  sleep 15
+
+  # and proceed with startup of the network-dependent tasks
+  puts "Network is available, proceeding..."
+  t = %x[sudo env RUBYLIB=/usr/local/lib/ruby/1.8:/usr/local/mcollective/lib \
+    mcollectived --config /usr/local/etc/mcollective/server.cfg \
+    --pidfile /var/run/mcollective.pid]
+
+elsif error_cond == RzNetworkUtils::TIMEOUT_EXCEEDED then
+
+  puts "Maximum wait time exceeded, network not found, exiting..."
+  exit(RzNetworkUtils::TIMEOUT_EXCEEDED)
+
+elsif error_cond == RzNetworkUtils::INVALID_IP_ADDRESS then
+
+  puts "DHCP address assignment failed, exiting..."
+  exit(RzNetworkUtils::INVALID_IP_ADDRESS)
+
+end
