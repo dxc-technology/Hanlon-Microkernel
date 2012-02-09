d7517116a195feb0ef32cc3f54aa49686ee5d8fe
Tom McSweeney <tjmcs@coastaccess.com>
2012-02-08 17:03:55 -0800

    Packaged up the Facter Agent code into a common subdirectory

---
+++ facter-agent/facteragent.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'facter'
+require 'yaml'
+
+module MCollective
+  module Agent
+    class Facteragent<RPC::Agent
+      metadata  :name        => "Facter Agent",
+                :description => "Preliminary Facter Agent",
+                :author      => "Tom McSweeney",
+                :license     => "Apache v2",
+                :version     => "1.0",
+                :url         => "http://www.emc.com",
+                :timeout     => 60
+
+      action "getall" do
+        # return the facts gathered by Facter (as a YAML-formatted string)
+        factMap = Hash.new
+	Facter.loadfacts
+        Facter.each { |fact, value|
+          factMap[fact.to_sym] = value
+        }
+        reply[:facts] = YAML.dump(factMap)
+      end
+      
+    end
+  end
+end
