25491e9bdbe4e7ed1c88a57c457faceca346a969
Tom McSweeney <tjmcs@coastaccess.com>
2012-03-07 02:51:46 -0800

    Refactored the "utility classes" in order to place them into their own module (the RazorMicrokernel module).  At the same time, added a "RazorMicrokernel::Logging" mixin that can be used to make a component loggable (this mixin is patterned after the ProjectRazor::Logging mixin in the Razor project).  Finally, made the changes necessary to replace the old "logger" code (where a Logger instance was created in the "main" scripts and passed into the associated methods) with the new mixin-based logger (where the capability to log is mixed into the scripts/classes).  The only difficulty was in sorting out how to apply the new mixin-based logger in the WEBrick instance, but that issue has been resolved.  This should complete the refactoring necessary to output logs in a manner that is equivalent to how logs are handled in the Razor project...

---
+++ razor_microkernel/logging.rb
@@ -0,0 +1,61 @@
+# EMC Confidential Information, protected under EMC Bilateral Non-Disclosure Agreement.
+# Copyright © 2012 EMC Corporation, All Rights Reserved
+
+require "logger"
+require "razor_microkernel/rz_mk_configuration_manager"
+
+LOG_LEVEL = Logger::DEBUG
+LOG_MAX_SIZE = 2048576
+LOG_MAX_FILES = 10
+
+# Module used for all logging. Needs to be included in any Razor Microkernel class that needs logging.
+# Uses Ruby Logger but overrides and instantiates one for each object that mixes in this module.
+# It auto prefixes each log message with classname and method from which it was called using progname
+module RazorMicrokernel::Logging
+
+  # [Hash] holds the loggers for each instance that includes it
+  @loggers = {}
+
+  # grab a reference to the configuration manager, use it to determine what level to log at (below)
+  @config_manager = (RazorMicrokernel::RzMkConfigurationManager).instance
+
+  # Returns the logger object specific to the instance that called it
+  def logger
+    classname = self.class.name
+    methodname = caller[0][/`([^']*)'/, 1]
+    @logger ||= RazorMicrokernel::Logging.logger_for(classname, methodname)
+    @logger.progname = "#{classname}\##{methodname}"
+    @logger
+  end
+
+  # Singleton override that returns a logger for each specific instance
+  class << self
+
+    def get_log_path
+      if !RZ_MK_LOG_PATH
+        "/var/log/rz_mk_common.log"
+      end
+      RZ_MK_LOG_PATH
+    end
+
+    def get_log_level
+      if !@config_manager.mk_log_level
+        return @config_manager.default_mk_log_level
+      end
+      @config_manager.mk_log_level
+    end
+
+    # Returns specific logger instance from loggers[Hash] or creates one if it doesn't exist
+    def logger_for(classname, methodname)
+      @loggers[classname] ||= configure_logger_for(classname, methodname)
+    end
+
+    # Creates a logger instance
+    def configure_logger_for(classname, methodname)
+      logger = Logger.new(get_log_path, shift_age = LOG_MAX_FILES, shift_size = LOG_MAX_SIZE)
+      logger.level = get_log_level
+      logger
+    end
+  end
+
+end
