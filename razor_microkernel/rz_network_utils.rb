5a943a3298e3a79f5e8b43788c6f8f8111792fe9
Tom McSweeney <tjmcs@coastaccess.com>
2012-09-06 14:39:24 -0700

    Shortened up timeout used for missing network to 2 minutes (was 5 minutes prior to this commit)

--- razor_microkernel/rz_network_utils.rb
+++ razor_microkernel/rz_network_utils.rb
@@ -9,7 +9,7 @@ module RazorMicrokernel
   class RzNetworkUtils
 
     # used internally
-    MAX_WAIT_TIME = 5 * 60    # wait for 5 minutes, max
+    MAX_WAIT_TIME = 2 * 60    # wait for 2 minutes, max
     WAIT_BETWEEN_ITER = 15    # wait 15 seconds between iterations
     DEF_ETH_PREFIX = "eth"
     SUCCESS = 0
