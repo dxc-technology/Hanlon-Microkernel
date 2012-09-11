fa916d31a390c7f22b5857cea8375e0d36ca1d41
Tom McSweeney <tjmcs@coastaccess.com>
2012-09-11 05:57:03 -0400

    Fixed a few typos in the last version (and moved the ifconfig output back to the top of the "each" rather than storing it as a separate variable and calling "each" on that list...that list is not used for anything else so it doesn't need to be kept in a separate variable).

--- razor_microkernel/rz_network_utils.rb
+++ razor_microkernel/rz_network_utils.rb
@@ -44,9 +44,6 @@ module RazorMicrokernel
       puts "Looking for network, this is attempt ##{prev_attempts + 1}"
       begin
 
-        # get the current ifconfig output
-        ifconfig_entries = %x[ifconfig].split("\n\n")
-
         # calculate the "wait_time" using an exponential backoff algorithm:
         #
         #      1/2 * (2**c - 1)
@@ -69,7 +66,7 @@ module RazorMicrokernel
               %x[sudo rmmod #{mod_name}; sudo modprobe #{mod_name}]
             end
           end
-          # if a NIC was found in the previous attemplt, but it doesn't have
+          # if a NIC was found in the previous attempt, but it doesn't have
           # a valid IP address, try restarting the DHCP client (to force another
           # DHCP request)
           unless nic_has_ip_addr
@@ -80,9 +77,8 @@ module RazorMicrokernel
 
         # loop through the ifconfig entries and search one ethernet NIC that
         # has a valid IP address
-        ifconfig_entries.each { |entry|
+        %x[ifconfig].split("\n\n").each { |entry|
 
-          # for each entry, check for an ethernet adapter (to eliminate
           # for each entry, check for an ethernet adapter (to eliminate
           # the loopback adapter) that has been assigned an IP address
           # and is in an "UP" state
