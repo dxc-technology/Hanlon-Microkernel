9dfa2a6e71a78d536777165a885f546cada692a8
Daniel Pittman <daniel@rimspace.net>
2012-12-13 17:12:01 -0800

    Avoid chroot when running the guest busybox
    Using the guest busybox is nice, but it is easier if we don't need to run it
    through chroot - especially when building under a fakeroot style environment.
    
    It already supports rebuilding a directory without having to chroot into it
    using the standard "alternate base" -b argument.
    
    The TCL guest busybox is a 32-bit, dynamically linked executable.  This causes
    some problems when built on a 64-bit box that doesn't have the 32-bit glibc
    installed - typically, a failure to execute with `No such file or directory`.
    
    This uses the guest libraries to execute the process, rather than sticking
    with the parent.
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

--- iso-build-files/rebuild_iso.sh
+++ iso-build-files/rebuild_iso.sh
@@ -34,17 +34,46 @@ else
     exit 1
 fi
 
+# We run the guest busybox using the host platform toolchain, so we should
+# test if it works or not.  We can't use a host busybox because some platforms
+# *cough* Ubuntu *cough* don't ship the depmod applet.
+#
+# This invocation is enough to run the guest Busybox on a 32-bit or 64-bit
+# kernel, without needing to depend on host libraries matching the guest, or
+# chroot, or anything super-fancy like that.
+BUSYBOX="env LD_LIBRARY_PATH=$PWD/tmp/lib:$LD_LIBRARY_PATH LD_PRELOAD="
+BUSYBOX="${BUSYBOX} $PWD/tmp/lib/ld-linux.so.2 $PWD/tmp/bin/busybox"
+if ! ${BUSYBOX} true; then
+    echo "It looks like I can't Unable to locate a working busybox for depmod!"
+    echo "I tried the TCL busybox too, and it failed even with chroot."
+    echo "If you see 'No such file or directory' above you probably need to install"
+    echo "the 32-bit libc on your machine!"
+    exit 1
+fi
+
 ISO_NAME=rz_mk_dev-image.${ISO_VERSION}.iso
 DIR_NAME=`pwd`
 set -x
 # build the YAML file in the Microkernel's filesystem that will be used to
 # display this same version information during boot
 ./add_version_to_mk_fs.rb tmp ${ISO_VERSION}
-# run chroot and ldconfig on the tmp directory (preparing it for construction
-# of a bootable core.gz file)
-#chroot ${DIR_NAME}/tmp depmod -a 3.0.21-tinycore
-chroot ${DIR_NAME}/tmp depmod -a `ls ${DIR_NAME}/extract/lib/modules`
-ldconfig -r ${DIR_NAME}/tmp
+
+# Run depmod and ldconfig on the tmp directory, preparing it for construction
+# of a bootable core.gz file in the event we added kernel modules or shared
+# libraries in our unpacking.
+#
+# We also replace an absolute symlink in the module directory with a relative
+# symlink, to allow depmod to correctly follow it without having to chroot.
+#
+# This is entirely compatible, but not done upstream, unfortunately.
+kernelver="$(ls ${DIR_NAME}/extract/lib/modules)"
+rm -f tmp/lib/modules/${kernelver}/kernel.tclocal
+ln -s ../../../usr/local/lib/modules/${kernelver}/kernel \
+    tmp/lib/modules/${kernelver}/kernel.tclocal
+
+${BUSYBOX} depmod -a -b ${DIR_NAME}/tmp ${kernelver}
+/sbin/ldconfig -r ${DIR_NAME}/tmp
+
 # build the new core.gz file (containing the contents of the tmp directory)
 cd tmp
 find | cpio -o -H newc | gzip -2 > ../core.gz
