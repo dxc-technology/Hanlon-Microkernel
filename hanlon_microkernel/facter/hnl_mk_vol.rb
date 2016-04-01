b125c3834146ffa83c5150d2b1c437122a464c76
Gerard Hickey <gerard.hickey@audiencescience.com>
2016-04-01 09:52:35 -0700

    Added counting facts to vol module

--- hanlon_microkernel/facter/hnl_mk_vol.rb
+++ hanlon_microkernel/facter/hnl_mk_vol.rb
@@ -17,11 +17,13 @@ lshw_cmd =  (virtual_type && virtual_type == 'kvm') ? 'lshw -disable dmi' : 'lsh
 lshw_c_volume_str = %x[sudo #{lshw_cmd} -c volume 2> /dev/null]
 
 # process the results from lshw -c volume
+vols = 0
 lshw_c_volume_str.split(/\s\s\*-/).each do |definition|
   unless definition.empty?
     lines = definition.split(/\n/)
     # section title is on the first line
-    volume = lines.shift.tr(':', '')
+    volume = lines.shift.tr(':', '') and vols += 1
+
     # Create a hash of attributes for each section (i.e. cpu)
     attribs = Hash[ lines.collect { |l| l =~ /^\s*([^:]+):\s+(.*)\s*$/; v=$2; [$1.gsub(/\s/, '_'), v] } ]
     attribs.each_pair do |attrib, val|
@@ -30,4 +32,9 @@ lshw_c_volume_str.split(/\s\s\*-/).each do |definition|
       end
     end
   end
+end
+
+# Report on the number volumes found
+Facter.add("mk_hw_volume_count") do
+  setcode { vols }
 end
